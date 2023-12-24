be6225a29c018a98fe766ad76eb38813160779bd
matsumoto <mty.to314_Kyo.re@icloud.com>
2023-12-24 10:33:08 +0000

    is_validを軽量化したmpposcriptを作成

--- MPPO_script_for_edit_1.py
+++ MPPO_script_for_edit_1.py
@@ -13,34 +13,23 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--nmax", type=int,default=14)
 parser.add_argument("--ntensor", type=int,default=6)
-parser.add_argument("--timesteps", type=int,default=200)
+parser.add_argument("--timesteps", type=int,default=2000000)
 parser.add_argument("--desc")
 args = parser.parse_args()
 
-max_legs = args.ntensor * 6
-num_leg_combinations=max_legs * (max_legs -1)//2
-terminal_state=np.zeros(args.ntensor+num_leg_combinations+1)
-terminal_state[-1] = 1
-
 #@profile
-def is_valid(state, action, env, num_max_actions=14, max_tensors=6, num_min_actions=0):
-    #state = np.array(state)
-    #out_state = np.zeros_like(state)
-    #terminal_state = np.zeros_like(state)
-    #terminal_state[-1] = 1
+def is_valid(state, action, env, num_max_actions, max_tensors, num_min_actions=0):
     if state[-1] == 1:
-        return False, terminal_state
-
-    # make sure not too many actions are taken
-    if np.sum(state) >= num_max_actions:
-        if action == env.action_space.n-1:
-            out_state = state.copy()
-            out_state[-1] = 1
-            return True, out_state
-        else:
-            return False, terminal_state
-    action_valid = True
+        return False
     num_tensor_actions = env.num_tensor_types * env.max_tensors
+    num_contractions = np.sum(state[num_tensor_actions:])
+    # make sure not too many actions are taken
+    if num_contractions >= num_max_actions:
+        if action == env.action_space.n-1:
+            return True
+        else:
+            return False
+    action_valid = True
     num_active_tensors = (state == 0).argmax(axis=0) # locates first 0 in the tensor arena
     num_legs_per_tensor = 5 # this will change later based on the environmnet
     num_active_legs = num_active_tensors  * num_legs_per_tensor
@@ -50,61 +39,36 @@ def is_valid(state, action, env, num_max_actions=14, max_tensors=6, num_min_acti
         tensor_idx, tensor_type = action // env.num_tensor_types, action % env.num_tensor_types
         if state[tensor_idx] != 0 or (tensor_idx != 0 and state[tensor_idx -1] == 0):
             action_valid = False
-            out_state = terminal_state
-        else:
-            out_state = state.copy()
-            out_state[tensor_idx] = 1 + tensor_type
     elif action < num_tensor_actions + env.num_leg_combinations:
         # leg contractions
         # check that there are no collisions
         (leg1, leg2), possible_conflicted_contractions = env.get_leg_indices_from_contraction_index(action - num_tensor_actions)
-        #print(len(possible_conflicted_contractions))
         #todo for文がないように書き換え
-        # for contraction_idx in possible_conflicted_contractions:
-        #     if state[contraction_idx + env.max_tensors] != 0:
-        #         action_valid = False
-        #         out_state = terminal_state
-        #         return action_valid, out_state
         conflict_indices = possible_conflicted_contractions + env.max_tensors
         if np.any(state[conflict_indices] != 0):
             action_valid = False
-            out_state = terminal_state
-            return action_valid, out_state     
+            return action_valid     
         # check that the legs in question are spawned by tensors
         if leg1 >= num_active_legs or leg2 >= num_active_legs:
             action_valid = False
-            out_state = terminal_state
         if leg1//env.tensor_size == leg2 //env.tensor_size:
             action_valid = False
-            out_state = terminal_state
-
-
         # check that it doesn't leave us with too few legs
-        num_contractions = np.sum(state[num_tensor_actions:])
         num_contracted_legs = 2 * num_contractions
         if num_active_legs - num_contracted_legs - 2 <= env.min_legs:
             action_valid = False
-            out_state = terminal_state
-
-        else:
-            out_state = state.copy()
-            out_state[action] = 1
     else:
         # terminating
-        if np.sum(state) < num_min_actions:
+        if num_contractions < num_min_actions:
             action_valid = False
-            out_state = terminal_state
-        else:
-            out_state = state.copy()
-            out_state[-1] = 1
 
-    return action_valid, out_state
+    return action_valid
 
 def mask_fn(env: gym.Env) -> np.ndarray:
     s = env.state
     mask = np.zeros(env.action_space.n)
     for a in range(env.action_space.n):
-        if is_valid(s, a, env=env, num_max_actions=args.nmax, max_tensors=args.ntensor)[0]:
+        if is_valid(s, a, env=env, num_max_actions=args.nmax, max_tensors=args.ntensor):
             mask[a] = True
     return np.array(mask)
 
